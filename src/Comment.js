f2b950faf1c23effa3311d1d65bb9333e8fd7d78
Leonardo Borchartt <leob_b@hotmail.com>
2019-12-17 00:05:39 +0000

    send

--- src/Comment.js
+++ src/Comment.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose');
 const CommentSchema = new mongoose.Schema({
     name:String,
     says:String,
+    localdate:String,
+    
 });
 module.exports = mongoose.model('Comment', CommentSchema);
