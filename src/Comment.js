302704d32bc4b35110c4db347b289b2954895cc7
Leonardo Borchartt <leob_b@hotmail.com>
2019-12-17 01:47:17 +0000

    send

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const dNow = new Date();
-const localdate1 = dNow.getDate() + '/' + (dNow.getMonth()+1) + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
 
 const CommentSchema = new mongoose.Schema({
     name:String,
